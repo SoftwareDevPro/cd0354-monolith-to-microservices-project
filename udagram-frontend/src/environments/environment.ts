d43f929b056483a52cc3d6204f9a7684847e4df4
Christopher Adamson <software.pro.08@gmail.com>
2022-04-21 11:51:32 -0700

    added screenshots + changes to frontend prod for endpoint

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a795a6de304cc4a54bbf768e0b935b35-1779731435.us-east-2.elb.amazonaws.com'
 };
 
 /*
